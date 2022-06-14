2d888cab77884d3fa806f78269ff7a7e8d699150
Haitham-Babbili <34158451+Haitham-Babbili@users.noreply.github.com>
2022-06-14 23:15:47 +0200

    Delete p2 directory

--- p2/src_generate.m
+++
@@ -1,8 +0,0 @@
-function output = src_generate(N)
-% This function is used to generate random information bit
-% Input
-% The input n is the length of this random inforamtion
-% Ouput
-% The output is random information bits of length N
-output = fix(rand(1,N)*2);
-end
