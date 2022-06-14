94b1c0668677a4271d29ac4e760434bc50c2f309
Haitham-Babbili <34158451+Haitham-Babbili@users.noreply.github.com>
2022-06-14 23:40:08 +0200

    Delete Project/convolution encoder:decoder of rate 1:n codes directory

--- Project/convolution encoder:decoder of rate 1:n codes/rate_1_N_convolution_code/compute_Hamm.m
+++
@@ -1,9 +0,0 @@
-function dist = compute_Hamm(o,r,block_st,n)
-%Function:  Computes the Hamming Distance.
-%o        - lth branch vector of the trellis map
-%r        - total received vector
-%block_st - branch currently on
-%n        - length of vector block
-
-rhat = r(block_st:block_st+n-1);  %Find r branch vector
-dist = sum(o ~= rhat);            %Computes Hamming Distance
